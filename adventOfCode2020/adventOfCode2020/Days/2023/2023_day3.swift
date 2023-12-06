52cbc48b58009cf56dd042fb6776ce7e9849b98d
Elena Skvortsova <skvortsovaeo@gmail.com>
2023-12-06 22:34:42 +0000

    2023 day 3 part 2

--- adventOfCode2020/adventOfCode2020/Days/2023/2023_day3.swift
+++ adventOfCode2020/adventOfCode2020/Days/2023/2023_day3.swift
@@ -20,62 +20,107 @@ extension Character {
         !isNumber && self != "."
     }
     
-    func isNotDot() -> Bool {
-        self != "."
+    func isMultiplier() -> Bool {
+        self == "*"
     }
 }
 
 extension Advent2023 {
-    static func getPartNumbers(from inputStrings: [String]) -> [Int] {
-        var numbers = [Int] ()
-        let stringArrays = inputStrings.map { Array($0) }
-        for (i, string) in stringArrays.enumerated() {
-            var number = [Character]()
-            var isPartNumber = false
-            for (k, character) in string.enumerated() {
-                if character.isNumber {
-                    number.append(character)
-                    if !isPartNumber,
-                       stringArrays[safe: i - 1]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
-                       stringArrays[safe: i - 1]?[safe: k]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i - 1]?[safe: k + 1]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i]?[safe: k + 1]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i + 1]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i + 1]?[safe: k]?.isNotNumberOrDot() ?? false ||
-                        stringArrays[safe: i + 1]?[safe: k + 1]?.isNotNumberOrDot() ?? false {
-                        isPartNumber = true
-                    }
-                        // debug print
-//                    print("""
-//\(stringArrays[safe: i - 1]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i - 1]?[safe: k] ?? "⬛️")\(stringArrays[safe: i - 1]?[safe: k + 1] ?? "⬛️")
-//\(stringArrays[safe: i]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i]?[safe: k] ?? "⬛️")\(stringArrays[safe: i]?[safe: k + 1] ?? "⬛️")
-//\(stringArrays[safe: i + 1]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i + 1]?[safe: k] ?? "⬛️")\(stringArrays[safe: i + 1]?[safe: k + 1] ?? "⬛️")
-//""")
-                } else {
-                    if !number.isEmpty {
-                        let numberInt = Int(String(number)) ?? 0
-                        if isPartNumber {
-                            numbers.append(numberInt)
+    
+    class GondolaEngineFix {
+        struct Coordinate: Hashable {
+            let x: Int
+            let y: Int
+        }
+        
+        private(set) var gears: [Coordinate: [Int]] = [:]
+        let partNumbers: [Int]
+        
+        init(inputStrings: [String]) {
+            var numbers = [Int] ()
+            let stringArrays = inputStrings.map { Array($0) }
+            for (i, string) in stringArrays.enumerated() {
+                var number = [Character]()
+                var isPartNumber = false
+                var gearCoordinates = [Coordinate]()
+                for (k, character) in string.enumerated() {
+                    if character.isNumber {
+                        number.append(character)
+                        if !isPartNumber,
+                           stringArrays[safe: i - 1]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i - 1]?[safe: k]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i - 1]?[safe: k + 1]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i]?[safe: k + 1]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i + 1]?[safe: k - 1]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i + 1]?[safe: k]?.isNotNumberOrDot() ?? false ||
+                            stringArrays[safe: i + 1]?[safe: k + 1]?.isNotNumberOrDot() ?? false {
+                            isPartNumber = true
+                            
+                            if stringArrays[safe: i - 1]?[safe: k - 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i - 1, y: k - 1))
+                            }
+                            if stringArrays[safe: i - 1]?[safe: k]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i - 1, y: k))
+                            }
+                            if stringArrays[safe: i - 1]?[safe: k + 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i - 1, y: k + 1))
+                            }
+                            if stringArrays[safe: i]?[safe: k - 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i, y: k - 1))
+                            }
+                            if stringArrays[safe: i]?[safe: k + 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i, y: k + 1))
+                            }
+                            if stringArrays[safe: i + 1]?[safe: k - 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i + 1, y: k - 1))
+                            }
+                            if stringArrays[safe: i + 1]?[safe: k]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i + 1, y: k))
+                            }
+                            if stringArrays[safe: i + 1]?[safe: k + 1]?.isMultiplier() ?? false {
+                                gearCoordinates.append(Coordinate(x: i + 1, y: k + 1))
+                            }
                         }
                         // debug print
-                        // print("\(isPartNumber ? "IS PART NUMBER:" : "NOT PART NUMBER") \(numberInt)")
+                        //                    print("""
+                        //\(stringArrays[safe: i - 1]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i - 1]?[safe: k] ?? "⬛️")\(stringArrays[safe: i - 1]?[safe: k + 1] ?? "⬛️")
+                        //\(stringArrays[safe: i]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i]?[safe: k] ?? "⬛️")\(stringArrays[safe: i]?[safe: k + 1] ?? "⬛️")
+                        //\(stringArrays[safe: i + 1]?[safe: k - 1] ?? "⬛️")\(stringArrays[safe: i + 1]?[safe: k] ?? "⬛️")\(stringArrays[safe: i + 1]?[safe: k + 1] ?? "⬛️")
+                        //""")
+                    } else {
+                        if !number.isEmpty {
+                            let numberInt = Int(String(number)) ?? 0
+                            if isPartNumber {
+                                numbers.append(numberInt)
+                                for coordinate in gearCoordinates {
+                                    gears[coordinate, default: []].append(numberInt)
+                                }
+                            }
+                            // debug print
+                            // print("\(isPartNumber ? "IS PART NUMBER:" : "NOT PART NUMBER") \(numberInt)")
+                        }
+                        number = []
+                        isPartNumber = false
+                        gearCoordinates = []
                     }
-                    number = []
-                    isPartNumber = false
                 }
-            }
-            if !number.isEmpty {
-                let numberInt = Int(String(number)) ?? 0
-                if isPartNumber {
-                    numbers.append(numberInt)
+                if !number.isEmpty {
+                    let numberInt = Int(String(number)) ?? 0
+                    if isPartNumber {
+                        numbers.append(numberInt)
+                        for coordinate in gearCoordinates {
+                            gears[coordinate, default: []].append(numberInt)
+                        }
+                    }
+                    // debug print
+                    // print("\(isPartNumber ? "IS PART NUMBER:" : "NOT PART NUMBER") \(numberInt)")
                 }
-                // debug print
-                // print("\(isPartNumber ? "IS PART NUMBER:" : "NOT PART NUMBER") \(numberInt)")
+                number = []
+                isPartNumber = false
+                gearCoordinates = []
             }
-            number = []
-            isPartNumber = false
+            self.partNumbers = numbers
         }
-        return numbers
     }
 }
--- adventOfCode2020/adventOfCode2020Tests/2023/2023day3.swift
+++ adventOfCode2020/adventOfCode2020Tests/2023/2023day3.swift
@@ -15,7 +15,8 @@ class AdventOfCode2023Day3: XCTestCase {
     func testDay3Test1() {
         let input = ResourceHelper.instance.getInput(from: "2023_day3_test1")
         let inputStrings = Advent2023.getInput(from: input)
-        let numbers = Advent2023.getPartNumbers(from: inputStrings)
+        let engineFix = Advent2023.GondolaEngineFix(inputStrings: inputStrings)
+        let numbers = engineFix.partNumbers
         let result = numbers.reduce(0, +)
         XCTAssertEqual(result, 4361)
     }
@@ -23,10 +24,34 @@ class AdventOfCode2023Day3: XCTestCase {
     func testDay3Input1() {
         let input = ResourceHelper.instance.getInput(from: "2023_day3_input1")
         let inputStrings = Advent2023.getInput(from: input)
-        let numbers = Advent2023.getPartNumbers(from: inputStrings)
+        let engineFix = Advent2023.GondolaEngineFix(inputStrings: inputStrings)
+        let numbers = engineFix.partNumbers
         let result = numbers.reduce(0, +)
         XCTAssertEqual(result, 527364)
     }
     
+    func testDay3Test2() {
+        let input = ResourceHelper.instance.getInput(from: "2023_day3_test1")
+        let inputStrings = Advent2023.getInput(from: input)
+        let engineFix = Advent2023.GondolaEngineFix(inputStrings: inputStrings)
+        let gears = engineFix.gears
+        let result: Int = gears.values.reduce(0) { partialResult, numbers in
+            print(numbers)
+            let sum = numbers.count > 1 ? numbers.reduce(1, *) : 0
+            return sum + partialResult
+        }
+        XCTAssertEqual(result, 467835)
+    }
     
+    func testDay3Input2() {
+        let input = ResourceHelper.instance.getInput(from: "2023_day3_input1")
+        let inputStrings = Advent2023.getInput(from: input)
+        let engineFix = Advent2023.GondolaEngineFix(inputStrings: inputStrings)
+        let gears = engineFix.gears
+        let result: Int = gears.values.reduce(0) { partialResult, numbers in
+            let sum = numbers.count > 1 ? numbers.reduce(1, *) : 0
+            return sum + partialResult
+        }
+        XCTAssertEqual(result, 79026871)
+    }
 }
