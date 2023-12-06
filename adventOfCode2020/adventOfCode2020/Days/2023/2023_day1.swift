d3ccba84c8fc6db22919980d4c9f346c1c7fc7bb
Elena Skvortsova <skvortsovaeo@gmail.com>
2023-12-06 11:13:06 +0000

    2023 day 1 part 2

--- adventOfCode2020/adventOfCode2020/Days/2023/2023_day1.swift
+++ adventOfCode2020/adventOfCode2020/Days/2023/2023_day1.swift
@@ -10,6 +10,28 @@ import Foundation
 struct Advent2023 {}
 
 extension Advent2023 {
+    
+    static var digits: [String: Int] = [
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9,
+        "enin" : 9,
+        "thgie" : 8,
+        "neves" : 7,
+        "xis" : 6,
+        "evif" : 5,
+        "ruof" : 4,
+        "eerht" : 3,
+        "owt" : 2,
+        "eno" : 1
+    ]
+    
     static func getInput(from input: String) -> [String] {
         return input.components(separatedBy: "\n")
     }
@@ -24,9 +46,28 @@ extension Advent2023 {
         return values
     }
     
-    static func getSumOfCalibrationValues(from input: String) -> Int {
-        let inputStrings = Advent2023.getInput(from: input)
+    static func getSumOfCalibrationValues(from inputStrings: [String]) -> Int {
         let values = Advent2023.getCalibrationValues(from: inputStrings)
         return values.reduce(0, +)
+    }
+    
+    static func getSumOfCalibrationValuesWithStringNumbers(from inputStirngs: [String]) -> Int {
+        let regex = /(one|two|three|four|five|six|seven|eight|nine|[0-9])/
+        let revertedRegex = /(enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|[0-9])/
+        let numbers = inputStirngs.compactMap { string -> Int? in
+            let firstMatch = string.firstMatch(of: regex)
+            guard let digit1Str = firstMatch else { return nil }
+            let d1 = String(digit1Str.0)
+            
+            let firstMatch2 = String(string.reversed()).firstMatch(of: revertedRegex)
+            guard let digit2Str = firstMatch2 else { return nil }
+            let d2 = String(digit2Str.0)
+            
+            guard let digit1 = Int(d1) ?? Advent2023.digits[d1],
+                  let digit2 = Int(d2) ?? Advent2023.digits[d2] else { return nil }
+
+            return digit1 * 10 + digit2
+        }
+        return numbers.reduce(0, +)
     }
 }
--- adventOfCode2020/adventOfCode2020Tests/2023/2023day1.swift
+++ adventOfCode2020/adventOfCode2020Tests/2023/2023day1.swift
@@ -12,14 +12,37 @@ class AdventOfCode2023Day1: XCTestCase {
 
     func testDay1Test1() {
         let input = ResourceHelper.instance.getInput(from: "2023_day1_test1")
-        let result = Advent2023.getSumOfCalibrationValues(from: input)
+        let inputStrings = Advent2023.getInput(from: input)
+        let result = Advent2023.getSumOfCalibrationValues(from: inputStrings)
         XCTAssertEqual(result, 142)
     }
 
     
     func testDay1Input1() {
-    let input = ResourceHelper.instance.getInput(from: "2023_day1_input1")
-    let result = Advent2023.getSumOfCalibrationValues(from: input)
-    XCTAssertEqual(result, 55488)
-}
+        let input = ResourceHelper.instance.getInput(from: "2023_day1_input1")
+        let inputStrings = Advent2023.getInput(from: input)
+        let result = Advent2023.getSumOfCalibrationValues(from: inputStrings)
+        XCTAssertEqual(result, 55488)
+    }
+    
+    func testDay1Test2() {
+        let input = ResourceHelper.instance.getInput(from: "2023_day1_test2")
+        let inputStrings = Advent2023.getInput(from: input)
+        let result = Advent2023.getSumOfCalibrationValuesWithStringNumbers(from: inputStrings)
+        XCTAssertEqual(result, 281)
+    }
+    
+    func testDay1Test3() {
+        let input = ResourceHelper.instance.getInput(from: "2023_day1_test3")
+        let inputStrings = Advent2023.getInput(from: input)
+        let result = Advent2023.getSumOfCalibrationValuesWithStringNumbers(from: inputStrings)
+        XCTAssertEqual(result, 54980)
+    }
+    
+    func testDay1Input2() {
+        let input = ResourceHelper.instance.getInput(from: "2023_day1_input1")
+        let inputStrings = Advent2023.getInput(from: input)
+        let result = Advent2023.getSumOfCalibrationValuesWithStringNumbers(from: inputStrings)
+        XCTAssertEqual(result, 55614)
+    }
 }
